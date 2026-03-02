a2b23946ce9d38bf64b00fcb781d90f8348199f8
chamodpw <mailtochamodw@gmail.com>
2026-03-02 22:54:58 +0530

    Update OpenMP examples: refine reduction documentation and optimize work-sharing code

--- openmp/workshare2.c
+++ openmp/workshare2.c
@@ -15,17 +15,17 @@
 int main(int argc, char *argv[])
 {
     int i, nthreads, tid;
-    float a[N], b[N], c[N], d[N];
+    float a[N], b[N], c[N];
 
     /* Some initializations */
     for (i = 0; i < N; i++)
     {
         a[i] = i * 1.5;
         b[i] = i + 22.35;
-        c[i] = d[i] = 0.0;
+        c[i] = 0.0;
     }
 
-#pragma omp parallel shared(a, b, c, d, nthreads) private(i, tid)
+#pragma omp parallel shared(a, b, c, nthreads) private(i, tid)
     {
         tid = omp_get_thread_num();
         if (tid == 0)
@@ -39,8 +39,8 @@ int main(int argc, char *argv[])
         {
 #pragma omp section
             {
-                printf("Thread %d doing section 1\n", tid);
-                for (i = 0; i < N; i++)
+                printf("Thread %d doing section 1 (addition)\n", tid);
+                for (i = 0; i < N/2; i++)
                 {
                     c[i] = a[i] + b[i];
                     printf("Thread %d: c[%d]= %f\n", tid, i, c[i]);
@@ -49,11 +49,11 @@ int main(int argc, char *argv[])
 
 #pragma omp section
             {
-                printf("Thread %d doing section 2\n", tid);
-                for (i = 0; i < N; i++)
+                printf("Thread %d doing section 2 (multiplication)\n", tid);
+                for (i = N/2; i < N; i++)
                 {
-                    d[i] = a[i] * b[i];
-                    printf("Thread %d: d[%d]= %f\n", tid, i, d[i]);
+                    c[i] = a[i] * b[i];
+                    printf("Thread %d: c[%d]= %f\n", tid, i, c[i]);
                 }
             }
 
